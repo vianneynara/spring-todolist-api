1389c1c2ae54dab11ce5603bcb79af191a233b0f
Nara <98963440+vianneynara@users.noreply.github.com>
2024-10-06 23:32:35 +0700

    feat: task finder by user id (as token)

--- src/main/java/dev/vianneynara/todolist/controller/TasksController.java
+++ src/main/java/dev/vianneynara/todolist/controller/TasksController.java
@@ -2,10 +2,14 @@ package dev.vianneynara.todolist.controller;
 
 import dev.vianneynara.todolist.entity.Tasks;
 import dev.vianneynara.todolist.service.TasksService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/api/v1")
 public class TasksController {
 
 	private final TasksService tasksService;
@@ -14,8 +18,30 @@ public class TasksController {
 		this.tasksService = tasksService;
 	}
 
-	@GetMapping("/tasks")
+	@GetMapping("/all-tasks")
 	public Iterable<Tasks> getTasks() {
 		return tasksService.findAll();
+	}
+
+	/**
+	 * Create get routing that asks for request header or body of user_id.
+	 * Will return all tasks that are associated with that user_id.
+	 * @param h_userId header `User-Identifier` that contains user id
+	 * @return Mapped JSON response.
+	 */
+	@GetMapping("/tasks")
+	public ResponseEntity<Map<String, List<Tasks>>> getTasksByUserId
+	(
+		@RequestHeader(value = "User-Identifier", required = true) Long h_userId
+	) {
+
+		if (h_userId == null) {
+			// return bad request if user id is not provided
+			return ResponseEntity.badRequest().build();
+		}
+
+		// query tasks that are associated with the user id
+		final List<Tasks> tasks = tasksService.findByUser_UserId(h_userId);
+		return ResponseEntity.ok(Map.of("data", tasks));
 	}
 }
--- src/main/java/dev/vianneynara/todolist/repository/TasksRepository.java
+++ src/main/java/dev/vianneynara/todolist/repository/TasksRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface TasksRepository extends CrudRepository<Tasks, Long> {
+
 	List<Tasks> findByUser_UserId(Long userId);
 }
