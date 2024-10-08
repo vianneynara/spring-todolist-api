b94a8927edbc830b47ca2c9bc570e966df68daa4
Nara <98963440+vianneynara@users.noreply.github.com>
2024-10-08 14:23:09 +0700

    refactor: enable JPA auditing

--- src/main/java/dev/vianneynara/todolist/SpringTodolistApiApplication.java
+++ src/main/java/dev/vianneynara/todolist/SpringTodolistApiApplication.java
@@ -1,22 +1,24 @@
 package dev.vianneynara.todolist;
 
-import dev.vianneynara.todolist.controller.TasksController;
-import dev.vianneynara.todolist.controller.UsersController;
+import dev.vianneynara.todolist.controller.TaskController;
+import dev.vianneynara.todolist.controller.AccountController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringTodolistApiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SpringTodolistApiApplication.class, args);
 
-		TasksController tasksController = ctx.getBean(TasksController.class);
-		UsersController usersController = ctx.getBean(UsersController.class);
+		TaskController taskController = ctx.getBean(TaskController.class);
+		AccountController accountController = ctx.getBean(AccountController.class);
 
-		System.out.println(tasksController.speak());
-		System.out.println(usersController.speak());
+		System.out.println(taskController.speak());
+		System.out.println(accountController.speak());
 	}
 
 }
