9e962124d82f99f5f95b472ba4c10b7879735f78
Nara <98963440+vianneynara@users.noreply.github.com>
2024-10-14 07:44:23 +0700

    feat: add CORS mapping

--- src/main/java/dev/vianneynara/todolist/SpringTodolistApiApplication.java
+++ src/main/java/dev/vianneynara/todolist/SpringTodolistApiApplication.java
@@ -5,7 +5,10 @@ import dev.vianneynara.todolist.controller.AccountController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -21,4 +24,13 @@ public class SpringTodolistApiApplication {
 		System.out.println(accountController.speak());
 	}
 
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1/**").allowedOrigins("*");
+			}
+		};
+    }
 }
---
+++ src/main/java/dev/vianneynara/todolist/configurations/WebConfig.java
@@ -0,0 +1,29 @@
+package dev.vianneynara.todolist.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig extends WebMvcConfigurationSupport {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**");
+//    }
+//}
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+}
