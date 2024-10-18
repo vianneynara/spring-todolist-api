39e60b7eb4cbf872221a7cda63b18a9096ed2826
Nara <98963440+vianneynara@users.noreply.github.com>
2024-10-18 08:35:22 +0700

    refactor: reformat business logic to use other service layers

--- src/main/java/dev/vianneynara/todolist/controller/AccountController.java
+++ src/main/java/dev/vianneynara/todolist/controller/AccountController.java
@@ -1,8 +1,8 @@
 package dev.vianneynara.todolist.controller;
 
+import dev.vianneynara.todolist.dto.AccountDto;
 import dev.vianneynara.todolist.entity.Account;
 import dev.vianneynara.todolist.exceptions.AccountExistsException;
-import dev.vianneynara.todolist.exceptions.AccountNotFoundException;
 import dev.vianneynara.todolist.exceptions.UnauthorizedException;
 import dev.vianneynara.todolist.service.AccountService;
 import org.springframework.http.HttpStatus;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -22,66 +23,71 @@ public class AccountController {
 		this.accountService = accountService;
 	}
 
-	/** Speaking method, used to test Spring Boot application context */
+	/**
+	 * Speaking method, used to test Spring Boot application context
+	 */
 	public String speak() {
 		return "I am ".concat(this.getClass().getName()) + "!";
 	}
 
 	/**
 	 * Gather all account in the database.
+	 *
 	 * @return all account.
 	 */
 	@GetMapping("/all-accounts")
-	public ResponseEntity<Object> getUsers(
-		@RequestHeader(name = "System-PIN") String h_systemPin
+	public ResponseEntity<Object> getAccounts(
+		@RequestHeader(name = "System-PIN") String systemPin
 	) {
-		final String systemPin = "todolist";
-		if (!(systemPin.equals(h_systemPin)))
-			throw new UnauthorizedException();
+		if (!"todolist".equals(systemPin)) {
+			throw new UnauthorizedException("Invalid system PIN");
+		}
 
 		List<Account> accounts = (List<Account>) accountService.findAll();
-		Set<Map<String, Object>> mappedAccounts = new HashSet<>();
-		for (Account account : accounts) {
-		   Map<String, Object> map = new LinkedHashMap<>();
-		   map.put("userId", account.getAccountId());
-		   map.put("username", account.getUsername());
-		   map.put("password", account.getPassword());
-		   map.put("tasks", account.getTasks().size());
-		   map.put("createdAt", account.getCreatedAt());
-		   map.put("updatedAt", account.getUpdatedAt());
-		   mappedAccounts.add(map);
-		}
-		return ResponseEntity.ok(mappedAccounts);
+		List<AccountDto> accountDtos = accounts.stream()
+			.map(this::convertToDto)
+			.collect(Collectors.toList());
+
+		return ResponseEntity.ok(accountDtos);
 	}
 
 	/**
 	 * POST routing that creates a new account.
+	 *
 	 * @param requestBody a JSON of `username` and `password`.
 	 * @return `OK` status for newly created resource, `CONFLICT` status if username already exists.
 	 */
 	@PostMapping("/accounts")
-	public ResponseEntity<Object> createAccount(
-		@RequestBody Map<String, Object> requestBody
+	public ResponseEntity<AccountDto> createAccount(
+		@RequestBody Map<String, String> requestBody
 	) {
-		// Checks whether the username already exists in the database
-		Optional<Account> existsAccount = accountService.findAccountByUsername((String) requestBody.get("username"));
-		if (existsAccount.isPresent())
-			throw new AccountExistsException();
+		String username = requestBody.get("username");
+		String password = requestBody.get("password");
+
+		if (accountService.accountExists(username)) {
+			throw new AccountExistsException("Account with username " + username + " already exists");
+		}
 
 		Account account = new Account();
-		account.setUsername((String) requestBody.get("username"));
-		account.setPassword((String) requestBody.get("password"));
+		account.setUsername(username);
+		account.setPassword(password);
 		account.setCreatedAt(LocalDateTime.now());
 		account.setUpdatedAt(LocalDateTime.now());
-		accountService.save(account);
 
-		final String TOKEN = "Bearer " + account.getAccountId();
-		return ResponseEntity.status(HttpStatus.CREATED).body(
-			Map.of(
-				"userId", account.getAccountId(),
-				"username", account.getUsername(),
-				"token", TOKEN
-			)
+		Account savedAccount = accountService.save(account);
+
+		AccountDto accountDto = convertToDto(savedAccount);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(accountDto);
+	}
+
+	private AccountDto convertToDto(Account account) {
+		return new AccountDto(
+			account.getAccountId(),
+			account.getUsername(),
+			account.getTasks().size(),
+			account.getCreatedAt(),
+			account.getUpdatedAt()
 		);
 	}
 }
--- src/main/java/dev/vianneynara/todolist/controller/AuthController.java
+++ src/main/java/dev/vianneynara/todolist/controller/AuthController.java
@@ -1,14 +1,11 @@
 package dev.vianneynara.todolist.controller;
 
-import dev.vianneynara.todolist.entity.Account;
-import dev.vianneynara.todolist.exceptions.AccountNotFoundException;
 import dev.vianneynara.todolist.service.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -29,16 +26,11 @@ public class AuthController {
 	public ResponseEntity<Object> login(
 		@RequestBody Map<String, Object> requestBody
 	) {
-		Optional<Account> accountQueryResult = accountService.findAccountByUsername((String) requestBody.get("username"));
-
-		// if the account is not found or the password is incorrect, return 404
-		if (accountQueryResult.isEmpty() || !accountQueryResult.get().getPassword().equals(requestBody.get("password"))) {
-			throw new AccountNotFoundException();
-		}
-
-		final String TOKEN = "Bearer " + accountQueryResult.get().getAccountId();
-		return ResponseEntity
-			.status(HttpStatus.OK)
+		final String TOKEN = accountService.requestToken(
+			(String) requestBody.get("username"),
+			(String) requestBody.get("password")
+		);
+		return ResponseEntity.status(HttpStatus.OK)
 			.header("Content-Type", "application/json")
 			.body(Map.of("token", TOKEN));
 	}
